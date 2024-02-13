b9a576d07e911b1bbc7dac24c9a865268e8fed7f
Eren <eren.livingstone@gmail.com>
2024-02-13 17:58:54 -0500

    Add more rock types, format constants

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,28 @@
 class_name Global
 
-enum RockType {DIRT, ROCK, IRON}
-const DAMAGE_TABLE = {RockType.DIRT: 1, RockType.ROCK: 5, RockType.IRON: 25}
-const ROCK_HEALTH_TABLE = {RockType.DIRT: 1, RockType.ROCK: 10, RockType.IRON: 50}
-const RESOURCE_TABLE = {RockType.DIRT: 5, RockType.ROCK: 10, RockType.IRON: 1}
+enum RockType {DIRT, ROCK, IRON, COPPER, ALUMINUM, COBALT}
+
+const DAMAGE_TABLE = {
+	RockType.DIRT: 1, 
+	RockType.ROCK: 5, 
+	RockType.IRON: 25, 
+	RockType.COPPER: 15, 
+	RockType.ALUMINUM: 10, 
+	RockType.COBALT: 50
+}
+const ROCK_HEALTH_TABLE = {
+	RockType.DIRT: 1, 
+	RockType.ROCK: 10, 
+	RockType.IRON: 50, 
+	RockType.COPPER: 50, 
+	RockType.ALUMINUM: 50, 
+	RockType.COBALT: 50
+}
+const RESOURCE_TABLE = {
+	RockType.DIRT: 5, 
+	RockType.ROCK: 10, 
+	RockType.IRON: 1,
+	RockType.COPPER: 5, 
+	RockType.ALUMINUM: 3, 
+	RockType.COBALT: 1
+}
