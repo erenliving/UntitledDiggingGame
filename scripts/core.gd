2ad891428ce85ebb52806da1b96ca8fa4270553e
Eren <eren.livingstone@gmail.com>
2024-02-13 17:59:08 -0500

    Clean up Core code

--- scripts/core.gd
+++ scripts/core.gd
@@ -9,14 +9,8 @@ class_name Core
 
 @onready var collision_shape_2d = $StaticBody2D/CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	(collision_shape_2d.shape as CircleShape2D).radius = radius
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _draw():
